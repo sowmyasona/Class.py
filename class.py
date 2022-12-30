e720778be9fd63dc04ff2b1ece418b2152fea5ce
sona@1027 <118789307+sowmyasona@users.noreply.github.com>
2022-12-30 22:32:02 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,17 @@
+class number():
+    x=2022
+    y=365
+print(number.x)
+print(number.x)
+b=number()
+print(b.x)
+print(b.y)
+
+class number():
+    x=21
+    y=12
+print(number.x)
+print(number.x)
+b=number()
+print(b.x)
+print(b.y)
